fbac67629fd91fe63666841f64611862c7c4121a
ktz <knightpop@gmail.com>
2017-07-30 23:25:56 +0900

    Ok

--- src/main/scala/org/example/ktz/hackerrank/recursion/TheSumsOfPowers.scala
+++ src/main/scala/org/example/ktz/hackerrank/recursion/TheSumsOfPowers.scala
@@ -3,34 +3,30 @@ package org.example.ktz.hackerrank.recursion
 
 object TheSumsOfPowers {
 
-  import scala.annotation.tailrec
+  implicit class RichList(list: List[Int]) {
+    def toPowerSum(pow: Int): Int = list.map(i => math.pow(i, pow)).sum.toInt
+  }
 
+  implicit class PowInt(int: Int) {
+    def toPow(pow: Int): Int = math.pow(int, pow).toInt
+  }
+
+  import scala.annotation.tailrec
   @tailrec
   def maxSquare(X: Int, N: Int, result: Int = 0): Int =
     if(math.pow(result + 1, N) > X) result
     else maxSquare(X, N, result + 1)
 
-  def takeN(elementList: List[Int], n: Int): List[List[Int]] = List.fill(n)(elementList)  // TODO 이거 아직 안 끝남!!
-
-
-  def makePowerSet(elementList: List[Int]): List[List[Int]] = {
-    @tailrec
-    def pwr(elementList: List[Int], powerSet: List[List[Int]]): List[List[Int]] =
-      if (elementList.isEmpty) powerSet
-      else pwr(elementList.tail, powerSet ++ (powerSet map (_ :+ elementList.head)))
-
-    val list = takeN(elementList, 3)
-
-
-    pwr(elementList, List(List.empty))
-  }
-
   def numberOfWays(X: Int, N: Int): Int = {
-    val powerSet = makePowerSet((maxSquare(X, N) to 1 by -1).toList)
+    def loop(current: Int, acc: List[Int]): List[List[Int]] =
+      if(acc.toPowerSum(N) > X)
+        Nil
+      else if (acc.toPowerSum(N) == X)
+        List(acc)
+      else
+        (current  to 1 by -1).toList.flatMap(i => loop(i - 1, i :: acc))
 
-    powerSet.count { partialSet =>
-      partialSet.map(ele => math.pow(ele, N)).sum == X
-    }
+    loop(maxSquare(X, N), List.empty).length
   }
 
   def main(args: Array[String]) {
