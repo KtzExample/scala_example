9ea49734ae167a0b1427ce2293c057b56b605bd9
ktz <knightpop@gmail.com>
2017-08-04 02:20:59 +0900

    blog!

---
+++ src/main/scala/org/example/ktz/hackerrank/day30/HelloWorld.scala
@@ -0,0 +1,14 @@
+package org.example.ktz.hackerrank.day30
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    // Print "Hello, World."
+    println("Hello, World.")
+
+    // Read a string variable
+    val s = scala.io.StdIn.readLine()
+
+    // Print the value of the string variable
+    println(s)
+  }
+}
